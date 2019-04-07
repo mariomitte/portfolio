4304e4cd5b87e8aa2257403efa047b8b141ea3c5
mariommite <mario_mitte@hotmail.com>
2019-04-07 21:55:40 +0200

    cleaning

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,5 @@
-import app from 'firebase/app';
-import 'firebase/database';
+import app from '@firebase/app';
+import '@firebase/database';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
