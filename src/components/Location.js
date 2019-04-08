65569478b3e459b55550c10bfa7fbf121d34c9aa
mariommite <mario_mitte@hotmail.com>
2019-04-08 08:00:34 +0200

    maps navbar circle

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,36 +2,61 @@ import React from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 class Location extends React.Component {
-  render() {
-    const { latitude, longitude } = this.props.geolocation
+  state = {
+    latitude: "",
+    longitude: "",
+    loaded: false,
+  }
 
-     return (
-      <LoadScript
-        id="script-loader"
-        googleMapsApiKey="AIzaSyD6y8E0P_jSDLcZKCjfE8VxRgq5My0SXVI"
-      >
-        <GoogleMap
-          id='map'
-          mapContainerStyle={{
-            height: "8rem",
-            width: "8rem",
-            borderRadius: "50%",
-            border: "0.224em solid white",
-            marginTop: "0.5rem",
-            pointerEvents: "none",
-          }}
-          zoom={8}
-          center={{
-            lat: latitude,
-            lng: longitude
-          }}
-          options={{
-            disableDefaultUI: "true"
-          }}
-        >
-        </GoogleMap>
-      </LoadScript>
-     )
+  componentDidMount() {
+    if(this.props.geolocation) {
+      let { latitude, longitude } = this.props.geolocation
+      this.setState({ latitude, longitude, loaded: true })
+    } else {
+      this.setState({ loaded: false })
+    }
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      latitude: "",
+      longitude: "",
+      loaded: false,
+    })
+  }
+
+  render() {
+    const { latitude, longitude, loaded } = this.state
+
+    return (
+      <React.Fragment>
+        {loaded && <LoadScript
+            id="script-loader"
+            googleMapsApiKey="AIzaSyD6y8E0P_jSDLcZKCjfE8VxRgq5My0SXVI"
+          >
+            <GoogleMap
+              id='map'
+              mapContainerStyle={{
+                height: "8rem",
+                width: "8rem",
+                borderRadius: "50%",
+                border: "0.224em solid white",
+                marginTop: "0.5rem",
+                pointerEvents: "none",
+              }}
+              zoom={8}
+              center={{
+                lat: latitude,
+                lng: longitude
+              }}
+              options={{
+                disableDefaultUI: "true"
+              }}
+            >
+            </GoogleMap>
+          </LoadScript>}
+      </React.Fragment>
+    )
   }
 }
 
