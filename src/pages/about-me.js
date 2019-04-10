1e349c6943ea0b5bbe1c27e51f41a468c5e6a23d
mariommite <mario_mitte@hotmail.com>
2019-04-10 07:57:44 +0200

    about me

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -2,35 +2,58 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Link, graphql } from "gatsby"
+import Img from 'gatsby-image'
 
-import { SEO, TemplateLayout } from '../components'
+import { SEO, TemplateLayout, Video } from '../components'
 import Header from '../components/Header';
 
 import website from '../../config'
 
 const AboutMe = ({ data: { prismicAboutMe }, location }) => {
   const { data } = prismicAboutMe
-  const image = data.image_header.localFile.childImageSharp.fluid
+  const image_header = data.image_header.localFile.childImageSharp.fluid
+  const zajedno_image = data.zajedno_image.localFile.childImageSharp.fluid
+
 
   return (
-    <TemplateLayout type={data.title.text} image={image}>
+    <TemplateLayout type={data.title.text} image={image_header}>
       <SEO
         title={`${data.title.text} | ${website.siteTitleAlt}`}
         pathname={location.pathname}
         node={prismicAboutMe}
-        image={image}
+        desc={website.siteDescription}
+        image={image_header}
       />
       <Wrapper>
         <Container>
           <PostList dangerouslySetInnerHTML={{ __html: data.content.html }} />
+        </Container>
+        <Video src={data.project_video.embed_url} />
+        <Container>
+          <PostList dangerouslySetInnerHTML={{ __html: data.project_content.html }} />
+        </Container>
+        <ImageContainer fluid={zajedno_image} />
+        <Container>
+          <PostList dangerouslySetInnerHTML={{ __html: data.zajedno_content.html }} />
         </Container>
       </Wrapper>
     </TemplateLayout>
   )
 }
 
+const ImageContainer = styled(Img)`
+  position: relative;
+  z-index: -1;
+  height: 100%;
+  width: 100%;
+`
+
 const Container = styled.div`
   padding: 0 2rem;
+
+  @media (max-width: 700px) {
+    padding: 0 0.5rem;
+  }
 `;
 
 const PostList = styled.div`
@@ -41,6 +64,7 @@ const PostList = styled.div`
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: flex-start;
   margin-bottom: 2rem;
@@ -79,6 +103,25 @@ export const blogQuery = graphql`
               }
             }
           }
+        }
+        project_video {
+          embed_url
+        }
+        project_content {
+          html
+        }
+        zajedno_image {
+          localFile {
+            childImageSharp {
+              fluid {
+                src
+                aspectRatio
+              }
+            }
+          }
+        }
+        zajedno_content {
+          html
         }
       }
     }
