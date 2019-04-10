214c1a0ce6d602ce9b311227b8f011dc724b062b
mariommite <mario_mitte@hotmail.com>
2019-04-10 18:38:54 +0200

    firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,9 +2,9 @@ import app from '@firebase/app';
 import '@firebase/database';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  apiKey: process.env.GATSBY_APP_API_KEY,
+  authDomain: process.env.GATSBY_APP_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_APP_DATABASE_URL,
 };
 
 console.log(config)
